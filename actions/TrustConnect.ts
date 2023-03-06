a143e357226f29c023acea5381f2effb8bf5fed0
Glitch <glitch.txs@gmail.com>
2023-03-06 05:08:19 -0300

    added workaround change chain

--- actions/TrustConnect.ts
+++ actions/TrustConnect.ts
@@ -7,15 +7,28 @@ export const TrustConnect = async()=>{
     const provider = await getTrustWalletInjectedProvider()
 
     if(Boolean(provider)){
-      await provider.request({ method: 'eth_requestAccounts' })
-      .then(()=> checkAccountAndChainId(provider))
-      .catch((err: any) => {
-        if (err.code === 4001) {
-          // EIP-1193 userRejectedRequest error
-          console.log('Trust Wallet: user rejected the connection request');
-        } else {
-          console.error('Trust Wallet: request connection error',err);
+
+        const requestConnection = async ()=>{
+            await provider.request({ method: 'eth_requestAccounts' })
+            .then(()=> checkAccountAndChainId(provider))
+            .catch((err: any) => {
+              if (err.code === 4001) {
+                // EIP-1193 userRejectedRequest error
+                console.log('Trust Wallet: user rejected the connection request');
+              } else {
+                console.error('Trust Wallet: request connection error',err);
+              }
+            });
         }
-      });        
+
+        await provider.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: '0x38' }],
+        }).then(requestConnection)
+        .catch((er: any)=>{
+          requestConnection()
+          console.error("Metamask: user rejected the add new chain request",er)
+        })
+        
     }
 }
