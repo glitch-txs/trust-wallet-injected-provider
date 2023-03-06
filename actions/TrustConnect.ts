bec62cceeb2614ec5e2b47e9be61f4674c3ca1c4
Glitch <glitch.txs@gmail.com>
2023-03-06 05:05:47 -0300

    remove switch chain when connecting

--- actions/TrustConnect.ts
+++ actions/TrustConnect.ts
@@ -7,25 +7,15 @@ export const TrustConnect = async()=>{
     const provider = await getTrustWalletInjectedProvider()
 
     if(Boolean(provider)){
-
-        const requestConnection = async ()=>{
-            await provider.request({ method: 'eth_requestAccounts' })
-            .then(()=> checkAccountAndChainId(provider))
-            .catch((err: any) => {
-              if (err.code === 4001) {
-                // EIP-1193 userRejectedRequest error
-                console.log('Trust Wallet: user rejected the connection request');
-              } else {
-                console.error('Trust Wallet: request connection error',err);
-              }
-            });
+      await provider.request({ method: 'eth_requestAccounts' })
+      .then(()=> checkAccountAndChainId(provider))
+      .catch((err: any) => {
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error
+          console.log('Trust Wallet: user rejected the connection request');
+        } else {
+          console.error('Trust Wallet: request connection error',err);
         }
-
-        await provider.request({
-            method: 'wallet_switchEthereumChain',
-            params: [{ chainId: '0x38' }],
-        }).then(requestConnection)
-        .catch((er: any)=>console.error("Metamask: user rejected the add new chain request",er))
-        
+      });        
     }
 }
